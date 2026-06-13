8c62b1ce9fda1ecd3f0ec645a313f6e3e1cd3cef
gatecodebpo-cmd <gatecodebpo@gmail.com>
2026-06-13 12:53:16 +0530

    all updates

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,11 +5,24 @@ dns.setServers(['1.1.1.1']);
 
 export const isDatabaseReady = () => mongoose.connection.readyState === 1;
 
+const sanitizeMongoUri = (uri) => {
+  try {
+    const url = new URL(uri.replace("mongodb+srv://", "mongodb://"));
+    const dbName = url.pathname.replace(/^\//, "");
+    if (dbName && /\s/.test(decodeURIComponent(dbName))) {
+      const cleanDbName = decodeURIComponent(dbName).replace(/\s+/g, "_");
+      return uri.replace(/\/[^/]+$/, `/${cleanDbName}`);
+    }
+  } catch {}
+  return uri;
+};
+
 export const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI;
-  if (!mongoUri) {
+  const rawUri = process.env.MONGO_URI;
+  if (!rawUri) {
     throw new Error("MONGO_URI is missing in environment variables.");
   }
+  const mongoUri = sanitizeMongoUri(rawUri);
   await mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 5000 });
   console.log("MongoDB connected");
 };
